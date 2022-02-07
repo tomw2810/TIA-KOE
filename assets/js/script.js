ce96fe607f08cb24af756d96460b76d046de8190
Tom Woodhouse <tomwoodhouse1991@gmail.com>
2022-02-07 21:11:50 +0000

    Output text inclusion for win, lose and draw functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -23,13 +23,25 @@ hair.addEventListener('click', function () {
     hairActive.innerHTML = 'hair!';
 })
 
+function runGame() {
+    pin.addEventListener('click', function () {
+        game('pin');
+    })
+    balloon.addEventListener('click', function () {
+        game('balloon');
+    })
+    hair.addEventListener('click', function () {
+        game('hair');
+    })
+};
+runGame();
+
 
 
 function userPicker(computerPick) {
     const choices = ['pin', 'balloon', 'hair'];
     if (choices === pin) {
         document.getElementById("user-pick").innerHTML = pin;
-        let result = document.innerHTML = ('I played pin');
     } else if (choices === balloon) {
         document.getElementById("user-pick").innerHTML = balloon;
     } else if (choices === hair) {
@@ -37,27 +49,26 @@ function userPicker(computerPick) {
     }
 }
 
+
+
 function loss() {
     compScore++;
     userScore_p.innerHTML = userScore;
     compScore_p.innerHTML = compScore;
+    document.getElementById('outcome-text').innerHTML = 'You lose that round!';
 };
 
 function draw() {
-
+    document.getElementById('outcome-text').innerHTML = 'You draw that round!';
 };
 
 function winner() {
     userScore++;
     userScore_p.innerHTML = userScore;
     compScore_p.innerHTML = compScore;
+    document.getElementById('outcome-text').innerHTML = 'You win that round!';
 };
 
-function gameEnd() {
-    if (userScore === 5 || compScore === 5) {
-        alert("The game has ended");
-    }
-}
 
 /* COMPUTER CHOICE */
 function computerPick() {
@@ -66,20 +77,21 @@ function computerPick() {
     return choices[randomNumber];
 }
 
-
-function game() {
+/*
+function game(userChoice) {
     const computerChoice = computerPick();
     console.log("user choice => " + userPick);
     console.log("computer pick => " + computerPick);
 }
-
+*/
+/*
 function gameEnd() {
     if (userScore === 5 || compScore === 5) {
         alert("The game has ended");
     };
     game();
 }
-
+*/
 
 function game(userChoice) {
     const computerChoice = computerPick();
@@ -103,18 +115,6 @@ function game(userChoice) {
 
 };
 
-function runGame() {
-    pin.addEventListener('click', function () {
-        game('pin');
-    })
-    balloon.addEventListener('click', function () {
-        game('balloon');
-    })
-    hair.addEventListener('click', function () {
-        game('hair');
-    })
-};
-runGame();
 
 function runGameComputer(game) {
     compvs.addEventListener('click', function () {
